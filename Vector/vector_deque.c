62a554471c51cea2ae8f493937f373acef1264b5
Aymen Kh <idev.aymen@gmail.com>
2019-05-21 12:35:41 -0700

    Latest Update

---
+++ Vector/vector_deque.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_deque.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 12:34:43 by akharrou          #+#    #+#             */
+/*   Updated: 2019/05/21 12:35:38 by akharrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**    NAME
+**         vector_deque -- brief
+**
+**    SYNOPSIS
+**         #include <libft.h>
+**
+**         void *
+**         vector_deque(struct s_vector *self);
+**
+**    PARAMETERS
+**
+**         struct s_vector *self     Pointer to a vector instance.
+**
+**    DESCRIPTION
+**         Description.
+**
+**    RETURN VALUES
+**         If successful returns the dequed element; otherwise NULL.
+*/
+
+#include "../Includes/vector.h"
+
+void		*vector_deque(struct s_vector *self)
+{
+	return (NULL);
+}
--- Vector/vector_popleft.c
+++ Vector/vector_popleft.c
@@ -6,7 +6,7 @@
 /*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 12:31:25 by akharrou          #+#    #+#             */
-/*   Updated: 2019/05/21 12:32:32 by akharrou         ###   ########.fr       */
+/*   Updated: 2019/05/21 12:34:27 by akharrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,10 @@
 **         struct s_vector *self     Pointer to a vector instance.
 **
 **    DESCRIPTION
-**         Pops off the (current) last element of the vector and
-**         returns it.
+**         Pops off the first element of the vector and returns it.
 **
-**         The index at which the element was is made to point to
-**         NULL.
+**         All elements that preceed the popped element are moved
+**         backwards by one index.
 **
 **    RETURN VALUES
 **         If successful returns the popped element; otherwise NULL.
