0032a285fdd50830cb2eaf9db77aed2b11fb2a7a
Aymen Kh <idev.aymen@gmail.com>
2019-06-07 03:59:39 -0700

    Latest Update

--- Unistd/ft_basename.c
+++ Unistd/ft_basename.c
@@ -6,19 +6,19 @@
 /*   By: akharrou <akharrou@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/07 02:23:57 by akharrou          #+#    #+#             */
-/*   Updated: 2019/06/07 02:26:36 by akharrou         ###   ########.fr       */
+/*   Updated: 2019/06/07 02:29:16 by akharrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/string_42.h"
 #include "../Includes/macros_42.h"
 
-void		ft_basename(const char *path, char *basename)
+void		ft_basename(const char *path, char *dest)
 {
 	char	**strtab;
 
 	strtab = ft_strsplit(path, "/");
 	ft_strncpy(
-		basename, strtab[ft_strtablen((const char**)strtab) - 1], MAX_NAMELEN);
+		dest, strtab[ft_strtablen((const char**)strtab) - 1], MAX_NAMELEN);
 	freestrtab(&strtab);
 }
